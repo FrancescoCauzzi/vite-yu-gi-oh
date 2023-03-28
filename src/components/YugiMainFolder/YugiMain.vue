8c1cc80893e109c24dcb53df302061fb838072df
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-03-28 20:37:42 +0200

    feat: tentativo di risolvere il bonus

--- src/components/YugiMainFolder/YugiMain.vue
+++ src/components/YugiMainFolder/YugiMain.vue
@@ -11,6 +11,7 @@ export default {
   name: "YugiMain",
   data() {
     return {
+      loading: false,
       store,
     };
   },
@@ -19,28 +20,52 @@ export default {
   },
 
   created() {
+    this.fetchData();
     // facciamo la chiamata API al caricamento della pagina
-
-    axios
-      .get("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=50&offset=0")
-      .then((res) => {
-        this.store.cards = res.data;
-        console.log(this.store.cards);
-      });
+    // axios
+    //   .get("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=50&offset=0")
+    //   .then((res) => {
+    //     this.store.cards = res.data;
+    //     console.log(this.store.cards);
+    //   });
+  },
+  methods: {
+    async fetchData() {
+      this.loading = true;
+      try {
+        const response = await axios.get(
+          "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=50&offset=0"
+        );
+        this.store.cards = response.data;
+      } catch (error) {
+        console.log(error);
+      }
+      this.loading = false;
+    },
   },
 };
 </script>
 <template>
   <div class="container-fluid __container-outer py-5">
-    <div class="container __cards-container">
-      <YugiMainCard
-        v-for="item in store.cards.data"
-        :card="item"
-      ></YugiMainCard>
+    <div class="container">
+      <div class="__loader" v-if="loading"></div>
+      <div class="__cards-container" v-else>
+        <YugiMainCard
+          v-for="item in store.cards.data"
+          :card="item"
+        ></YugiMainCard>
+      </div>
     </div>
   </div>
 </template>
 <style scoped>
+.__loader {
+  background-image: url(/img/yugi-bg.jpg);
+  background-size: contain;
+  background-repeat: no-repeat;
+  width: 100%;
+  height: 1000px;
+}
 .__container-outer {
   background-image: url(/img/bg-custom-img.jpg);
   background-repeat: repeat;
