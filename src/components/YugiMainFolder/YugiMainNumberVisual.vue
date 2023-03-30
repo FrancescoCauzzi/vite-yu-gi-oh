fab8357ae48da4b0fbfd916ced45fa3354e9c7d9
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-03-30 11:02:34 +0200

    feat: piccole modifiche durante correz

--- src/components/YugiMainFolder/YugiMainNumberVisual.vue
+++ src/components/YugiMainFolder/YugiMainNumberVisual.vue
@@ -12,8 +12,9 @@ export default {
 <template>
   <div class="__visualizer mb-3 container bg-dark text-white w-50 fw-bold py-2">
     <span>
-      There are {{ store.cardsLength }} cards that correspond to your
-      query</span
+      There {{ store.cardsLength === 1 ? "is" : "are" }}
+      {{ store.cardsLength }} card{{ store.cardsLength === 1 ? "" : "s" }} that
+      correspond to your query</span
     >
   </div>
 </template>
